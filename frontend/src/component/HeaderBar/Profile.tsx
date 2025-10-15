61a134ba34e2fcd6391324b71331d2cea81f46ab
Cyron <reskriptii@gmail.com>
2025-10-16 06:42:39 +0700

    add user data fetching login on page's profile button

--- frontend/src/component/HeaderBar/Profile.tsx
+++ frontend/src/component/HeaderBar/Profile.tsx
@@ -1,23 +1,52 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ProfileMenu } from "./ProfileMenu";
+import type { user } from "./type";
 
 export default function Profile() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<user | null>(null);
   const [isMenuCollasped, setIsMenuCollasped] = useState(true);
 
+  useEffect(() => {
+    let isMount = true;
+
+    axios
+      .get(window.api + "/auth/me", { withCredentials: true })
+      .then((res) => {
+        if (isMount) setUser(res.data);
+      });
+
+    return () => {
+      isMount = false;
+    };
+  }, []);
   return (
     <div className="relative">
-      <button
-        className="relative overflow-hidden rounded-full"
-        onClick={() => setIsMenuCollasped(!isMenuCollasped)}
-      >
-        <img className="size-12 bg-amber-100" alt="profile picture" />
-        <div className="absolute top-0 left-0 size-full hover:bg-black/10"></div>
-      </button>
+      <ProfileButton
+        user={user}
+        onClick={() => {
+          setIsMenuCollasped(!isMenuCollasped);
+        }}
+      />
       <div className="absolute">
         <ProfileMenu hidden={isMenuCollasped} />
       </div>
     </div>
+  );
+}
+
+type ProfileButtonArgs = { user: user | null; onClick: () => void };
+function ProfileButton({ user, onClick }: ProfileButtonArgs) {
+  const profilePicUrl = user
+    ? window.api + `/user/${user.id}/profileimg`
+    : "/profile.svg";
+  return (
+    <button
+      className="relative size-12 overflow-hidden rounded-full"
+      onClick={onClick}
+    >
+      <img className="size-12" alt="profile picture" src={profilePicUrl} />
+      <div className="absolute top-0 left-0 size-full hover:bg-black/10"></div>
+    </button>
   );
 }
---
+++ frontend/src/component/HeaderBar/type.d.ts
@@ -0,0 +1,6 @@
+export type user = {
+  id: number;
+  name: string;
+  email: string;
+  role: "user" | "librarian" | "admin";
+};
