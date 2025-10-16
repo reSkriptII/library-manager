53fb6e3c1de296eecebecdef01e6b8d939c7c309
Cyron <reskriptii@gmail.com>
2025-10-17 04:52:20 +0700

    add get book with id endpoint

--- backend/src/route/book/bookRoute.ts
+++ backend/src/route/book/bookRoute.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import { sendBooks } from "./controller/sendBooks.js";
 import { sendCoverImg } from "./controller/sendCoverImg.js";
+import { sendBook } from "./controller/sendBook.js";
 
 const bookRoute = express.Router();
 
 bookRoute.get("/", sendBooks);
+bookRoute.get("/:id", sendBook);
 bookRoute.get("/:id/cover", sendCoverImg);
 
 export { bookRoute };
---
+++ backend/src/route/book/controller/sendBook.ts
@@ -0,0 +1,30 @@
+import { psqlPool } from "#util/db.js";
+import { sendResponse } from "#util/sendResponse.js";
+import type { Request, Response } from "express";
+
+export async function sendBook(req: Request, res: Response) {
+  const bookId = req.params.id;
+
+  try {
+    const result = await psqlPool.query(
+      `SELECT title, author, availability 
+        FROM books
+        WHERE book_id = $1`,
+      [bookId]
+    );
+
+    const bookData = result.rows[0];
+    if (!bookData) {
+      return sendResponse(res, false, 404, "book not found");
+    }
+
+    return sendResponse(res, true, {
+      id: bookId,
+      title: bookData.title,
+      author: bookData.author,
+      available: bookData.availability,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
