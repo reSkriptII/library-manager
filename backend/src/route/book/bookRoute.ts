45ae56c8300ce35f0da8aa188c020dd4302e65d5
Cyron <reskriptii@gmail.com>
2025-10-18 23:57:46 +0700

    add return book endpoint

--- backend/src/route/book/bookRoute.ts
+++ backend/src/route/book/bookRoute.ts
@@ -6,12 +6,14 @@ import { sendCoverImg } from "./controller/sendCoverImg.js";
 import { sendBook } from "./controller/sendBook.js";
 import { addBook } from "./controller/addBook.js";
 import { borrowBook } from "./controller/borrowBook.js";
+import { returnBook } from "./controller/returnBook.js";
 
 const upload = multer({ dest: "./coverimage/" });
 const bookRoute = express.Router();
 
 bookRoute.post("/add", upload.single("coverimg"), addBook);
 bookRoute.post("/borrow", borrowBook);
+bookRoute.post("/return", returnBook);
 
 bookRoute.get("/", sendBooks);
 bookRoute.get("/:id", sendBook);
