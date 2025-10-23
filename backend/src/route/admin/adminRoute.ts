b3fd485ebc9482b0b3405d152ec2d0fdc772beb2
Cyron <reskriptii@gmail.com>
2025-10-24 03:23:59 +0700

    add admin/addauthor and addgenre endpoint

--- backend/src/route/admin/adminRoute.ts
+++ backend/src/route/admin/adminRoute.ts
@@ -4,16 +4,16 @@ import multer from "multer";
 import { addBook } from "./controller/addBook.js";
 import { authenticate } from "middleware/authenticate.js";
 import { checkRole } from "middleware/checkRole.js";
+import { addAuthor } from "./controller/addAuthor.js";
+import { addGenre } from "./controller/addGenre.js";
 
 const upload = multer({ dest: "./coverimage/" });
 const adminRoute = express.Router();
 
-adminRoute.post(
-  "/addbook",
-  authenticate,
-  checkRole("admin"),
-  upload.single("coverimg"),
-  addBook
-);
+adminRoute.use(authenticate, checkRole("admin"));
+
+adminRoute.post("/addbook", upload.single("coverimg"), addBook);
+adminRoute.post("/addauthor", addAuthor);
+adminRoute.post("/addgenre", addGenre);
 
 export { adminRoute };
