11b03fdca516d3e776e5e0e74854b0e879f35848
Cyron <reskriptii@gmail.com>
2025-11-02 22:03:34 +0700

    type structure of controller functions

--- backend/src/feature/books/books.types.ts
+++ backend/src/feature/books/books.types.ts
@@ -1,8 +1,9 @@
 import type { Middleware } from "types/express.js";
+import { editBook } from "./books.controllers.js";
 
 export namespace Books {
   //#region Book retrieval
-  export namespace getBooksList {
+  export namespace GetBooksList {
     export type ReqQuery = {
       title?: string;
       genre?: number | number[];
@@ -24,9 +25,52 @@ export namespace Books {
       reserveQueue: number;
     }[];
 
-    export type controller = Middleware<{}, ReqQuery, undefined, ResBody>;
+    export type Controller = Middleware<{}, ReqQuery, undefined, ResBody>;
   }
 
-  export namespace getBookById {}
+  export namespace GetBookById {
+    export type ReqParam = { id: number };
+
+    export type ResBody = {
+      id: number;
+      title: string;
+      genres: {
+        id: number;
+        name: string;
+      }[];
+      author: {
+        id: number;
+        name: string;
+      }[];
+      lent: boolean;
+      reserveQueue: number;
+    } | null;
+
+    export type Controller = Middleware<ReqParam, {}, undefined, ResBody>;
+  }
   //#endregion
+  //#region Book modification
+  export namespace CreateBook {
+    export type ReqBody = {
+      title: string;
+      genres: number[];
+      authors: number[];
+    };
+
+    export type Controller = Middleware<{}, {}, ReqBody>;
+  }
+
+  export namespace EditBook {
+    export type ReqBody = {
+      title?: string;
+      genres?: number[];
+      authors?: number[];
+    };
+
+    export type Controller = Middleware<{}, {}, ReqBody>;
+  }
+  //#endregion
+
+  export type CreateAuthorController = Middleware<{}, {}, { author: string }>;
+  export type CreateGenreController = Middleware<{}, {}, { genre: string }>;
 }
