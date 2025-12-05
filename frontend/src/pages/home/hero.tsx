312de7b68dfa6334edf54eb95c120876d789b1a2
Cyron <reskriptii@gmail.com>
2025-12-06 01:33:07 +0700

    fix hero background image

--- frontend/src/pages/home/hero.tsx
+++ frontend/src/pages/home/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
   const { user } = useUser();
 
   return (
-    <div className="relative left-1/2 w-screen -translate-x-1/2 border-b bg-[url('herobackground.webp')] bg-cover">
+    <div className="relative left-1/2 w-screen -translate-x-1/2 border-b bg-[url('/herobackground.webp')] bg-cover">
       <div className="container mx-auto px-8 py-12">
         <h1 className="mb-8 text-2xl text-black">
           Welcome to <span className="text-3xl font-bold">The Library</span>
