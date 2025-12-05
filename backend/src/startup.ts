c234d3caf9a9acc62e82e4a997fb00e340988106
Cyron <reskriptii@gmail.com>
2025-12-06 01:02:22 +0700

    add directory creation on backend startup

--- backend/src/startup.ts
+++ backend/src/startup.ts
@@ -1,3 +1,9 @@
-import { loadEnv } from "./config/env.js";
+import fs from "fs";
+import { loadEnv, ENV } from "./config/env.js";
 
+console.log("Loading environment variable");
 loadEnv();
+
+console.log("Creating storage directory");
+fs.mkdirSync(ENV().AVATAR_IMAGE_DIR_PATH, { recursive: true });
+fs.mkdirSync(ENV().COVER_IMAGE_DIR_PATH, { recursive: true });
